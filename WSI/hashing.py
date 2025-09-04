f5598b43d1d30f132f4a821aadaf66a675898e38
Seyed Mostafa M. Kahaki <mousavikahaki@gmail.com>
2025-09-04 10:28:10 -0400

    update duplicate detection
    update duplicate detection and add affect of different rotation and resolution

--- WSI/hashing.py
+++ WSI/hashing.py
@@ -19,7 +19,7 @@ def normalized_hamming_distance_hex(hash1, hash2):
 
     return hamming_distance / max_distance
 
-def phash_level(filepath, level=0, rotate_degrees=None):
+def phash_level(filepath, level=0, rotate_degrees=None, resize_percentage=None):
     with tifffile.TiffFile(filepath) as tif:
         page = tif.pages[level]
         arr = page.asarray()
@@ -27,15 +27,26 @@ def phash_level(filepath, level=0, rotate_degrees=None):
 
     if rotate_degrees is not None:
         img = img.rotate(rotate_degrees, expand=False, fillcolor=(255, 255, 255))
+    
+    if resize_percentage is not None:
+        #new_size = (800, 600)  # Example: 800 pixels wide, 600 pixels high
         
+        original_width, original_height = img.size
+        new_width = int(original_width * (resize_percentage / 100))
+        new_height = int(original_height * (resize_percentage / 100))
+        new_size = (new_width, new_height)
+        print(new_size)
+        img = img.resize(new_size)
+    #plt.imshow(img)
+    #plt.show()    
     return imagehash.phash(img)
 
-def calculate_hamming_distance(first_wsi, second_wsi, rotation=0):
+def calculate_hamming_distance(first_wsi, second_wsi, rotation=0, resizepercentage = 100):
     print(f"\n{'Comparison':40} {'Hash 1':<32} {'Hash 2':<32} {'Hamming Distance'}")
     print("-" * 120)
     
-    first_hash  = phash_level(first_wsi,  level=2, rotate_degrees=rotation)
-    second_hash = phash_level(second_wsi, level=2, rotate_degrees=rotation)
+    first_hash  = phash_level(first_wsi,  level=1)
+    second_hash = phash_level(second_wsi, level=1, rotate_degrees=rotation, resize_percentage = resizepercentage)
 
     table_data  = []
 
